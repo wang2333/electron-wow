c947396330c39942e3ab563e276f4fafe88f46a7
wang liang <296290877@qq.com>
2024-10-23 22:21:26 +0800

    更新调试

--- ahk/autokey.py
+++ ahk/autokey.py
@@ -9,152 +9,153 @@ import pygame
 import os
 from mss import mss  # 导入mss库
 import numpy as np  # 导入numpy用于高效的数组操作
+import random
 
 # 从TypeScript文件中提取的颜色到按键的映射
 COLOR_TO_KEY_MAP: Dict[str, str] = {
-    '#8cd6ee': 'altleft-ctrlleft-f1',
-    '#8ca3ee': 'altleft-ctrlleft-f2',
-    '#9b8cee': 'altleft-ctrlleft-f3',
-    '#bd8ded': 'altleft-ctrlleft-f4',
-    '#e48cee': 'altleft-ctrlleft-f5',
-    '#ee8cdb': 'altleft-ctrlleft-f6',
-    '#ef8fbd': 'altleft-ctrlleft-f7',
-    '#ef8f92': 'altleft-ctrlleft-f8',
-    '#e673c4': 'altleft-ctrlleft-f9',
-    '#9571e8': 'altleft-ctrlleft-f10',
-    '#e7ae18': 'altleft-ctrlleft-f11',
-    '#e19257': 'altleft-ctrlleft-f12',
+    '#8cd6ee': 'alt-ctrl-f1',
+    '#8ca3ee': 'alt-ctrl-f2',
+    '#9b8cee': 'alt-ctrl-f3',
+    '#bd8ded': 'alt-ctrl-f4',
+    '#e48cee': 'alt-ctrl-f5',
+    '#ee8cdb': 'alt-ctrl-f6',
+    '#ef8fbd': 'alt-ctrl-f7',
+    '#ef8f92': 'alt-ctrl-f8',
+    '#e673c4': 'alt-ctrl-f9',
+    '#9571e8': 'alt-ctrl-f10',
+    '#e7ae18': 'alt-ctrl-f11',
+    '#e19257': 'alt-ctrl-f12',
 
-    '#f5f9b7': 'altleft-ctrlleft-leftshift-f1',
-    '#f9ddb7': 'altleft-ctrlleft-leftshift-f2',
-    '#f7c2ae': 'altleft-ctrlleft-leftshift-f3',
-    '#f7aeae': 'altleft-ctrlleft-leftshift-f4',
-    '#eda08d': 'altleft-ctrlleft-leftshift-f5',
-    '#edb98d': 'altleft-ctrlleft-leftshift-f6',
-    '#edda8d': 'altleft-ctrlleft-leftshift-f7',
-    '#d2ed8d': 'altleft-ctrlleft-leftshift-f8',
-    '#a6ed8d': 'altleft-ctrlleft-leftshift-f9',
-    '#8dedce': 'altleft-ctrlleft-leftshift-f10',
-    '#31e4f9': 'altleft-ctrlleft-leftshift-f11',
-    '#8a88d0': 'altleft-ctrlleft-leftshift-f12',
+    '#f5f9b7': 'alt-ctrl-shift-f1',
+    '#f9ddb7': 'alt-ctrl-shift-f2',
+    '#f7c2ae': 'alt-ctrl-shift-f3',
+    '#f7aeae': 'alt-ctrl-shift-f4',
+    '#eda08d': 'alt-ctrl-shift-f5',
+    '#edb98d': 'alt-ctrl-shift-f6',
+    '#edda8d': 'alt-ctrl-shift-f7',
+    '#d2ed8d': 'alt-ctrl-shift-f8',
+    '#a6ed8d': 'alt-ctrl-shift-f9',
+    '#8dedce': 'alt-ctrl-shift-f10',
+    '#31e4f9': 'alt-ctrl-shift-f11',
+    '#8a88d0': 'alt-ctrl-shift-f12',
 
-    '#aee6ae': 'altleft-0',
-    '#bd4043': 'altleft-1',
-    '#dc96c7': 'altleft-2',
-    '#b54ac4': 'altleft-3',
-    '#bd9bdf': 'altleft-4',
-    '#5444c4': 'altleft-5',
-    '#9eb7e0': 'altleft-6',
-    '#57b4c8': 'altleft-7',
-    '#97ddcf': 'altleft-8',
-    '#4fc882': 'altleft-9',
+    '#aee6ae': 'alt-num0',
+    '#bd4043': 'alt-num1',
+    '#dc96c7': 'alt-num2',
+    '#b54ac4': 'alt-num3',
+    '#bd9bdf': 'alt-num4',
+    '#5444c4': 'alt-num5',
+    '#9eb7e0': 'alt-num6',
+    '#57b4c8': 'alt-num7',
+    '#97ddcf': 'alt-num8',
+    '#4fc882': 'alt-num9',
 
-    '#f8b4b6': 'altleft-leftshift-f1',
-    '#f9b3db': 'altleft-leftshift-f2',
-    '#fab1f4': 'altleft-leftshift-f3',
-    '#e2b3f9': 'altleft-leftshift-f4',
-    '#cbb1fa': 'altleft-leftshift-f5',
-    '#b0b3fb': 'altleft-leftshift-f6',
-    '#b0d2fb': 'altleft-leftshift-f7',
-    '#b3ecf9': 'altleft-leftshift-f8',
-    '#b7f9d8': 'altleft-leftshift-f9',
-    '#b7f9c2': 'altleft-leftshift-f10',
-    '#56b3da': 'altleft-leftshift-f11',
-    '#a669c7': 'altleft-leftshift-f12',
+    '#f8b4b6': 'alt-shift-f1',
+    '#f9b3db': 'alt-shift-f2',
+    '#fab1f4': 'alt-shift-f3',
+    '#e2b3f9': 'alt-shift-f4',
+    '#cbb1fa': 'alt-shift-f5',
+    '#b0b3fb': 'alt-shift-f6',
+    '#b0d2fb': 'alt-shift-f7',
+    '#b3ecf9': 'alt-shift-f8',
+    '#b7f9d8': 'alt-shift-f9',
+    '#b7f9c2': 'alt-shift-f10',
+    '#56b3da': 'alt-shift-f11',
+    '#a669c7': 'alt-shift-f12',
 
-    '#f78a48': 'altleft-ctrlleft-0',
-    '#f9e746': 'altleft-ctrlleft-1',
-    '#bffa45': 'altleft-ctrlleft-2',
-    '#6dfa45': 'altleft-ctrlleft-3',
-    '#44fbbf': 'altleft-ctrlleft-4',
-    '#44c4fb': 'altleft-ctrlleft-5',
-    '#4476fb': 'altleft-ctrlleft-6',
-    '#9644fb': 'altleft-ctrlleft-7',
-    '#f744fb': 'altleft-ctrlleft-8',
-    '#fb4496': 'altleft-ctrlleft-9',
+    '#f78a48': 'alt-ctrl-num0',
+    '#f9e746': 'alt-ctrl-num1',
+    '#bffa45': 'alt-ctrl-num2',
+    '#6dfa45': 'alt-ctrl-num3',
+    '#44fbbf': 'alt-ctrl-num4',
+    '#44c4fb': 'alt-ctrl-num5',
+    '#4476fb': 'alt-ctrl-num6',
+    '#9644fb': 'alt-ctrl-num7',
+    '#f744fb': 'alt-ctrl-num8',
+    '#fb4496': 'alt-ctrl-num9',
 
-    '#59a323': 'ctrlleft-f1',
-    '#35483c': 'ctrlleft-f2',
-    '#b89eb8': 'ctrlleft-f3',
-    '#b19eb8': 'ctrlleft-f4',
-    '#ab9eb8': 'ctrlleft-f5',
-    '#a59eb8': 'ctrlleft-f6',
-    '#9e9eb8': 'ctrlleft-f7',
-    '#9eb8b8': 'ctrlleft-f8',
-    '#b89eb1': 'ctrlleft-f9',
-    '#9eb1b8': 'ctrlleft-f10',
-    '#9eb89e': 'ctrlleft-f11',
-    '#b8b89e': 'ctrlleft-f12',
+    '#59a323': 'ctrl-f1',
+    '#35483c': 'ctrl-f2',
+    '#b89eb8': 'ctrl-f3',
+    '#b19eb8': 'ctrl-f4',
+    '#ab9eb8': 'ctrl-f5',
+    '#a59eb8': 'ctrl-f6',
+    '#9e9eb8': 'ctrl-f7',
+    '#9eb8b8': 'ctrl-f8',
+    '#b89eb1': 'ctrl-f9',
+    '#9eb1b8': 'ctrl-f10',
+    '#9eb89e': 'ctrl-f11',
+    '#b8b89e': 'ctrl-f12',
 
-    '#e66f68': 'ctrlleft-0',
-    '#e7aa67': 'ctrlleft-1',
-    '#e8d166': 'ctrlleft-2',
-    '#dce965': 'ctrlleft-3',
-    '#9de965': 'ctrlleft-4',
-    '#22cc1e': 'ctrlleft-5',
-    '#40e3aa': 'ctrlleft-6',
-    '#40cbe3': 'ctrlleft-7',
-    '#3f75e4': 'ctrlleft-8',
-    '#743ee6': 'ctrlleft-9',
+    '#e66f68': 'ctrl-num0',
+    '#e7aa67': 'ctrl-num1',
+    '#e8d166': 'ctrl-num2',
+    '#dce965': 'ctrl-num3',
+    '#9de965': 'ctrl-num4',
+    '#22cc1e': 'ctrl-num5',
+    '#40e3aa': 'ctrl-num6',
+    '#40cbe3': 'ctrl-num7',
+    '#3f75e4': 'ctrl-num8',
+    '#743ee6': 'ctrl-num9',
 
-    '#f154da': 'ctrlleft-leftshift-f1',
-    '#e0a3f8': 'ctrlleft-leftshift-f2',
-    '#8d56f3': 'ctrlleft-leftshift-f3',
-    '#9c9cf8': 'ctrlleft-leftshift-f4',
-    '#60a3f4': 'ctrlleft-leftshift-f5',
-    '#b4f8fa': 'ctrlleft-leftshift-f6',
-    '#6cf4aa': 'ctrlleft-leftshift-f7',
-    '#c6fab4': 'ctrlleft-leftshift-f8',
-    '#d7f777': 'ctrlleft-leftshift-f9',
-    '#fac2da': 'ctrlleft-leftshift-f10',
-    '#a56fbf': 'ctrlleft-leftshift-f11',
-    '#83a34e': 'ctrlleft-leftshift-f12',
+    '#f154da': 'ctrl-shift-f1',
+    '#e0a3f8': 'ctrl-shift-f2',
+    '#8d56f3': 'ctrl-shift-f3',
+    '#9c9cf8': 'ctrl-shift-f4',
+    '#60a3f4': 'ctrl-shift-f5',
+    '#b4f8fa': 'ctrl-shift-f6',
+    '#6cf4aa': 'ctrl-shift-f7',
+    '#c6fab4': 'ctrl-shift-f8',
+    '#d7f777': 'ctrl-shift-f9',
+    '#fac2da': 'ctrl-shift-f10',
+    '#a56fbf': 'ctrl-shift-f11',
+    '#83a34e': 'ctrl-shift-f12',
 
-    '#ecb182': 'ctrlleft-leftshift-b',
-    '#a6f4f2': 'ctrlleft-leftshift-c',
-    '#a6f4c1': 'ctrlleft-leftshift-f',
-    '#f4cfa6': 'ctrlleft-leftshift-g',
-    '#ece782': 'ctrlleft-leftshift-h',
-    '#82c7ec': 'ctrlleft-leftshift-i',
-    '#81edda': 'ctrlleft-leftshift-j',
-    '#b183eb': 'ctrlleft-leftshift-k',
-    '#ea84b9': 'ctrlleft-leftshift-l',
-    '#8393eb': 'ctrlleft-leftshift-m',
-    '#81eda2': 'ctrlleft-leftshift-n',
-    '#ea84ea': 'ctrlleft-leftshift-o',
-    '#ea849b': 'ctrlleft-leftshift-p',
-    '#c9f4a6': 'ctrlleft-leftshift-t',
-    '#baec82': 'ctrlleft-leftshift-u',
-    '#f4f2a6': 'ctrlleft-leftshift-v',
-    '#a6c4f4': 'ctrlleft-leftshift-x',
-    '#f4aaa6': 'ctrlleft-leftshift-y',
-    '#bba6f4': 'ctrlleft-leftshift-z',
+    '#ecb182': 'ctrl-shift-b',
+    '#a6f4f2': 'ctrl-shift-c',
+    '#a6f4c1': 'ctrl-shift-f',
+    '#f4cfa6': 'ctrl-shift-g',
+    '#ece782': 'ctrl-shift-h',
+    '#82c7ec': 'ctrl-shift-i',
+    '#81edda': 'ctrl-shift-j',
+    '#b183eb': 'ctrl-shift-k',
+    '#ea84b9': 'ctrl-shift-l',
+    '#8393eb': 'ctrl-shift-m',
+    '#81eda2': 'ctrl-shift-n',
+    '#ea84ea': 'ctrl-shift-o',
+    '#ea849b': 'ctrl-shift-p',
+    '#c9f4a6': 'ctrl-shift-t',
+    '#baec82': 'ctrl-shift-u',
+    '#f4f2a6': 'ctrl-shift-v',
+    '#a6c4f4': 'ctrl-shift-x',
+    '#f4aaa6': 'ctrl-shift-y',
+    '#bba6f4': 'ctrl-shift-z',
 
-    '#28232c': 'leftshift-f1',
-    '#b8ab9e': 'leftshift-f2',
-    '#3f3d4e': 'leftshift-f3',
-    '#b8a59e': 'leftshift-f4',
-    '#b8b19e': 'leftshift-f5',
-    '#30373f': 'leftshift-f6',
-    '#9eabb8': 'leftshift-f7',
-    '#9ea5b8': 'leftshift-f8',
-    '#9eb8b1': 'leftshift-f9',
-    '#b89eab': 'leftshift-f10',
-    '#b1b89e': 'leftshift-f11',
-    '#abb89e': 'leftshift-f12',
+    '#28232c': 'shift-f1',
+    '#b8ab9e': 'shift-f2',
+    '#3f3d4e': 'shift-f3',
+    '#b8a59e': 'shift-f4',
+    '#b8b19e': 'shift-f5',
+    '#30373f': 'shift-f6',
+    '#9eabb8': 'shift-f7',
+    '#9ea5b8': 'shift-f8',
+    '#9eb8b1': 'shift-f9',
+    '#b89eab': 'shift-f10',
+    '#b1b89e': 'shift-f11',
+    '#abb89e': 'shift-f12',
 
-    '#8e4065': 'altleft-f1',
-    '#c477b6': 'altleft-f2',
-    '#8c4aac': 'altleft-f3',
-    '#8c76c7': 'altleft-f4',
-    '#4552ab': 'altleft-f5',
-    '#81bcc5': 'altleft-f6',
-    '#58b474': 'altleft-f7',
-    '#9fd599': 'altleft-f8',
-    '#9ebe5c': 'altleft-f9',
-    '#989138': 'altleft-f10',
-    '#dcbda0': 'altleft-f11',
-    '#7e3a3a': 'altleft-f12',
+    '#8e4065': 'alt-f1',
+    '#c477b6': 'alt-f2',
+    '#8c4aac': 'alt-f3',
+    '#8c76c7': 'alt-f4',
+    '#4552ab': 'alt-f5',
+    '#81bcc5': 'alt-f6',
+    '#58b474': 'alt-f7',
+    '#9fd599': 'alt-f8',
+    '#9ebe5c': 'alt-f9',
+    '#989138': 'alt-f10',
+    '#dcbda0': 'alt-f11',
+    '#7e3a3a': 'alt-f12',
 
     '#795e4d': 'f5',
     '#49623e': 'f6',
@@ -166,7 +167,7 @@ COLOR_TO_KEY_MAP: Dict[str, str] = {
     '#1e1c68': 'f12',
 
     '#a5b89e': 'combat',
-    '#b6b485': 'ctrlleft-shiftleft-home',
+    '#b6b485': 'ctrl-shiftleft-home',
     '#b89e9f': 'stop'
 }
 
@@ -231,7 +232,7 @@ class AutoKeyController:
                     screenshot = self.sct.grab(self.monitor)
                     img = np.array(screenshot)
                     pixel_color = img[0, self.current_x - self.monitor["left"]][:3]
-                    color_key = f"{pixel_color[0]},{pixel_color[1]},{pixel_color[2]}"
+                    color_key = f"{pixel_color[2]},{pixel_color[1]},{pixel_color[0]}"
                     if color_key in self.color_map:
                         key_combination = self.color_map[color_key]
                         self.execute_key_press(key_combination)
@@ -242,23 +243,21 @@ class AutoKeyController:
     def execute_key_press(self, key_combination):
         print(f"触发按键: {key_combination}")
         keys = key_combination.split('-')
-        for key in keys:
-            pyautogui.keyDown(key)
-        time.sleep(0.01)
-        for key in reversed(keys):
-            pyautogui.keyUp(key)
+        interval = random.uniform(0.05, 0.1)
+        pyautogui.hotkey(*keys, interval=interval)
+
 
 def on_press(key, controller):
     try:
-        if key == keyboard.Key.f1:
+        if key.char == '1':
             if not controller.running:
                 controller.toggle()
             controller.switch_to_x1()
-        elif key == keyboard.Key.f2:
+        elif key.char == '2':
             if not controller.running:
                 controller.toggle()
             controller.switch_to_x2()
-        elif key == keyboard.Key.f3:
+        elif key.char == '3':
             controller.toggle()
     except AttributeError:
         pass
@@ -273,9 +272,9 @@ def signal_handler(signum, frame):
 
 if __name__ == "__main__":
     # 设置要监控的屏幕坐标
-    MONITOR_X1 = 100  # 请根据实际需要调整这个值
-    MONITOR_X2 = 200  # 请根据实际需要调整这个值
-    MONITOR_Y = 100   # 请根据实际需要调整这个值
+    MONITOR_X1 = 1  # 请根据实际需要调整这个值
+    MONITOR_X2 = 2550  # 请根据实际需要调整这个值
+    MONITOR_Y = 25   # 请根据实际需要调整这个值
 
     controller = AutoKeyController(MONITOR_X1, MONITOR_X2, MONITOR_Y)
 
